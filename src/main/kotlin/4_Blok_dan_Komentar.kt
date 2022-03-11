12c423c5f0a151de6c7d761b10e154a540626334
Gina <ginasahiba@gmail.com>
2022-03-11 15:49:03 +0700

    percobaan Blok dan Komentar

---
+++ src/main/kotlin/4_Blok_dan_Komentar.kt
@@ -0,0 +1,12 @@
+// mempelajari 3 macam bentuk blok dan komentar
+
+fun main() {
+    println("Hello, Ini program pertama saya di kotlin!")
+    //contoh komentar satu baris
+    /*
+    contoh komentar multi baris
+     */
+    /**
+     * Kotlin Doc --> baris ini untuk mendokumentasikan kode program
+     */
+}
